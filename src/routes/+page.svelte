7a126e21e9d6b64afec8801f1e7f389bde5d2fcd
nop <noplab90@gmail.com>
2024-09-29 19:05:42 +0900

    add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="flex items-center justify-between px-6 py-4 bg-gray-900">
+	<h1 class="text-2xl font-bold text-white">nop's blog</h1>
+	<nav class="flex gap-4">
+		<a href="/" class="text-white hover:text-gray-400 p-1 hover:bg-slate-800">home</a>
+		<a href="/profiles" class="text-white hover:text-gray-400 p-1 hover:bg-slate-800">Profile</a>
+	</nav>
+</header>
+
+<footer class="bg-black py-2 text-center text-white">
+	<p>My Awesome Website!</p>
+</footer>
